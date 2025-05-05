19090dfa954caa3322029167be66702c2e83e80e
sonic kuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-05-05 21:18:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = int(environ.get("API_ID", "22215080")) #Replace with your api id
+API_ID = environ.get("API_ID", "22215080") #Replace with your api id
 API_HASH = environ.get("API_HASH", "6ab80ad5d78fee18fdd9b909edfbafd5") #Replace with your api hash
 BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
