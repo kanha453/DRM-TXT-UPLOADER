f5f2f8f4ca9ec2cf80d35bd765fc6da6da24f6d2
kanha453 <meenakanhaiyalal2025@gmail.com>
2025-06-18 19:04:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 from os import environ
 
+
+# first comend
+# git clone https://github.com/kanha453/DRM-TXT-UPLOADER
+# cd DRM-TXT-UPLOADER
+
+# senad comend
+# sudo apt install docker.io -y
+# sudo docker build -t text-leech-bot-by-ashu .
+# sudo docker run -it --rm text-leech-bot-by-ashu
+
 API_ID = environ.get("API_ID", "24473318") #Replace with your api id
 API_HASH = environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5") #Replace with your api hash
 BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
